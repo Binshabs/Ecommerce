bd36f1328a2aed344489d1965fb521c13b042572
Binshabs <binshabs567@gmail.com>
2024-02-14 13:07:43 +0530

    new file

--- Todoproject/cartapp/urls.py
+++ Todoproject/cartapp/urls.py
@@ -1,13 +1,10 @@
-from django.urls import path
 from .import views
+from django.urls import path
 app_name='cartapp'
 urlpatterns = [
     path('addcart/<int:id>',views.addcart,name='addcart'),
     path('removecart/<int:id>',views.removecart,name='removecart'),
     path('fullremove/<int:id>',views.fullremove,name='fullremove'),
-    path('placeorder/<int:id>',views.placeorder,name='placeorder'),
-    
-    
-    
     path('displaycart/',views.displaycart,name='displaycart'),
+    path('placeorder/',views.placeorder,name='placeorder'),
 ]
